e87c1b48b4cade1d0c15565e1b6ba4be1f2a0a73
Aisha Nair <AishaNair@users.noreply.github.com>
2016-09-05 17:16:23 +0530

    Add files via upload

---
+++ Virus/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Thread;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading.Timer;
+namespace Virus
+{
+    
+    public partial class Ghost : Form
+    {
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi)]
+        protected static extern int mciSendString(string lpstrCommand, StringBuilder lpstrReturnString, int uReturnLength, IntPtr hwndCallback);
+        
+        public Ghost()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //method
+        public bool ProcessCDTray(bool open) 
+        {
+            int ret = 0;
+            //do a switch of the value passed
+            switch (open)
+            {
+                case true: //true = open the cd 
+                ret = mciSendString("set cdaudio door open", null, 0, IntPtr.Zero);
+                return true;
+                break;
+                case false: //false = close the tray 
+                ret = mciSendString("set cdaudio door closed", null, 0, IntPtr.Zero);
+                return true; 
+                break;
+                default: ret = mciSendString("set cdaudio door open", null, 0, IntPtr.Zero); return true; break; 
+            }
+           }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <5; i++)
+            {
+                ProcessCDTray(true);
+                ProcessCDTray(false);
+                //System.Threading.Thread.Sleep(5000);
+                
+             }
+        }
+      }
+}
